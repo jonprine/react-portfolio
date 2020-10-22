d01fd306f27d4b09cbee39c5e97a48247c9c401c
Vox Humana <jonprine@gmail.com>
2020-10-22 11:16:05 -0500

    fixed nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { useState } from 'react';
+// import Header from './components/Header';
+import About from './components/About';
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  
+  const [categories] = useState([
+    {
+      name: 'projects',
+      description: 'Applications completed during the Full Stack course',
+    },
+    { name: 'resume', description: 'Resume' }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Header></Header>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        <div> {!contactSelected ? (
+            <> 
+        <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+          </> 
+          ) : 
+            (
+            <ContactForm></ContactForm>
+            
+          )}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Projects/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Modal from "../Modal";
+
+const Projects = ({ category }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const [photos] = useState([
+        {
+            name: 'Bone Apple Tea',
+            // image: require('../../assets/boneappletea.png'),
+            deployedLink: 'https://tranquil-ridge-22533.herokuapp.com/',
+            repoLink: 'https://github.com/KRAY306090/project-two',
+            description: 'An online recipe database that allows users to login and share their own receipes and comment on others.'
+        },
+        {
+            name: 'News By the Map',
+            // image: require('../../assets/newsbythemap.png'),
+            deployedLink: 'https://adamkeyser45.github.io/newsbythemap/',
+            repoLink: 'https://github.com/adamkeyser45/newsbythemap',
+            description: 'Users can search news specific to a google maps search of a city.'
+        },
+        {
+            name: 'Tech Blog',
+            // image: require('../../assets/techblog.png'),
+            deployedLink: 'https://serene-falls-33864.herokuapp.com/',
+            repoLink: 'https://github.com/jonprine/tech-blog',
+            description: 'Users can log in and post, comment on a tech message board.'
+        },
+        {
+            name: 'Weather Dashboard',
+            // image: require('../../assets/weather.png'),
+            deployedLink:'https://jonprine.github.io/weather-dashboard/',
+            repoLink: 'https://github.com/jonprine/weather-dashboard',
+            description: 'User can search 5 day weather forecast for a particular city.'
+        },
+        {
+            name: 'Social Media App',
+            // image: require('../../assets/screenshot3.png'),
+            deployedLink: 'https://github.com/jonprine/social-media',
+            repoLink: 'https://github.com/jonprine/social-media',
+            description: 'Social network api using MongoDB and Mongoose.'
+        },
+        {
+            name: 'Coding Quiz',
+            // image: require('../../assets/codingquiz.png'),
+            deployedLink: 'https://jonprine.github.io/coding-quiz/',
+            repoLink: 'https://github.com/jonprine/coding-quiz',
+            description: 'A timed quiz to test your knowledge of JavaScript.'
+        }
+    ]);
+
+    const currentPhotos = photos.filter((photo) => photo.category === category);
+
+    const [currentPhoto, setCurrentPhoto] = useState();
+  
+   
+
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(!isModalOpen);
+      }
+
+      return (
+        <div>
+          {isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal} />)}
+          <div className="flex-row">
+            {currentPhotos.map((image, i) => (
+              <img
+                src={require(`../../assets/${category}/${i}.png`)}
+                alt={image.name}
+                className="img-thumbnail mx-1"
+                key={image.name}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    };
+
+export default Projects;

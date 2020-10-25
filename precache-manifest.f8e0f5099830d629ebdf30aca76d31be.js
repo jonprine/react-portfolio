ea538749db0c5f1d78c869c0119a151f60d7c54d
Vox Humana <jonprine@gmail.com>
2020-10-25 09:36:42 -0500

    Updates

---
+++ precache-manifest.f8e0f5099830d629ebdf30aca76d31be.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc106f6abed0987f9a6d12d0496d2ed",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "aa4e1ed5c1f19db202a7",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4ff794d257f4ce44bee8",
+    "url": "/react-portfolio/static/css/main.e929d365.chunk.css"
+  },
+  {
+    "revision": "aa4e1ed5c1f19db202a7",
+    "url": "/react-portfolio/static/js/2.ed0104a1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.ed0104a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff794d257f4ce44bee8",
+    "url": "/react-portfolio/static/js/main.f5433e27.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "688ace80288fdceaba6e1b9d7cd9d83c",
+    "url": "/react-portfolio/static/media/0.688ace80.png"
+  },
+  {
+    "revision": "52b4772d9d929eb8c83a49e2ade12513",
+    "url": "/react-portfolio/static/media/1.52b4772d.png"
+  },
+  {
+    "revision": "8eb7fab8ddc35e8304b1c5e7db9bd36c",
+    "url": "/react-portfolio/static/media/2.8eb7fab8.png"
+  },
+  {
+    "revision": "cf559f3c9341848b555720b45d0fc870",
+    "url": "/react-portfolio/static/media/3.cf559f3c.png"
+  },
+  {
+    "revision": "f4f6f45f59abe3009ddfe524408ca042",
+    "url": "/react-portfolio/static/media/4.f4f6f45f.png"
+  },
+  {
+    "revision": "c1dac6d7c01377de1021c1f99e5c9889",
+    "url": "/react-portfolio/static/media/5.c1dac6d7.png"
+  },
+  {
+    "revision": "c616701d0fb29682249047cc22e55908",
+    "url": "/react-portfolio/static/media/profilepic.c616701d.jpg"
+  },
+  {
+    "revision": "c5782c8333cc027af38c2c5a83ab0510",
+    "url": "/react-portfolio/static/media/resume.c5782c83.pdf"
+  },
+  {
+    "revision": "f460244f221b2cf3ac8daf6a728a29b4",
+    "url": "/react-portfolio/static/media/resume.f460244f.png"
+  },
+  {
+    "revision": "a29614d3cf5b229372c3a047a38299e2",
+    "url": "/react-portfolio/static/media/screenshot1.a29614d3.png"
+  },
+  {
+    "revision": "4d4aa803f330d1276525bdbcf4070ebc",
+    "url": "/react-portfolio/static/media/screenshot2.4d4aa803.png"
+  },
+  {
+    "revision": "eda15b401b1f22e77be68af55269a02b",
+    "url": "/react-portfolio/static/media/screenshot3.eda15b40.png"
+  }
+]);
